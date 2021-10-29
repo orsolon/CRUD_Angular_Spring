136935c7d600e588a59d17f40dde45271bf41ab7
orsolon <56239735+orsolon@users.noreply.github.com>
2021-10-29 14:52:30 -0300

    add spinner and http get

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { delay, first, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      {_id: '1', name:'Angular', category: 'front-end'},
-      {_id: '2', name:'React', category: 'front-end'},
-      {_id: '3', name:'.Net', category: 'backend'},
-    ]
+  list() {
+      return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+        first(),
+        delay(5000),
+        tap(courses => console.log(courses))
+      );
+  
   }
 }
